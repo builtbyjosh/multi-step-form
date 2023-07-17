b0075111d192e45086e36a43c41b10b5a8e7b072
builtbyjosh <joshuacollins912@gmail.com>
2023-07-16 20:31:24 -0500

    update styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -17,22 +17,9 @@ const Sidebar = () => {
     setActiveIndex(index);
   };
   return (
-    <Box
-      bgImage={bgImage}
-      bgSize={'contain'}
-      bgRepeat={'no-repeat'}
-      m={4}
-      width="274px"
-      height="568px"
-      aspectRatio={'3/4'}
-    >
-      <Box pt={5} ml={8}>
-        <UnorderedList
-          spacing={12}
-          styleType={'none'}
-          fontSize={'sm'}
-          textAlign={'left'}
-        >
+    <Box bgImage={bgImage} maxWidth="274px" w={'100%'} aspectRatio={'274/568'}>
+      <Box pt={10} ml={5}>
+        <UnorderedList spacing={8} styleType={'none'} textAlign={'left'}>
           <SidebarItem
             step={1}
             text={'YOUR INFO'}
@@ -67,11 +54,15 @@ export default Sidebar;
 
 const SidebarItem = ({ step, text, activeIndex, handleActiveItem }) => {
   return (
-    <ListItem onClick={() => handleActiveItem(step)} cursor="pointer">
+    <ListItem
+      onClick={() => handleActiveItem(step)}
+      cursor="pointer"
+      w={'fit-content'}
+    >
       <Stack direction={'row'} align={'center'} color={'light-gray'}>
         <Circle
           border={'1px'}
-          size={'25px'}
+          size={'30px'}
           bgColor={activeIndex === step ? 'light-blue' : ''}
           fontWeight={'bold'}
         >
@@ -92,7 +83,7 @@ const SidebarItem = ({ step, text, activeIndex, handleActiveItem }) => {
           <Text fontSize={'xs'} color={'cool-gray'}>
             STEP {step}
           </Text>
-          <Text color={'White'} fontWeight={'semibold'}>
+          <Text color={'White'} fontSize={'sm'} fontWeight={'semibold'}>
             {text}
           </Text>
         </Stack>
