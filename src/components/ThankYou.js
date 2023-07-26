79cb90d87fed1f81e7ebb5e90ef6b241df24d3f4
builtbyjosh <joshuacollins912@gmail.com>
2023-07-26 13:00:39 -0500

    finished css

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,15 +1,23 @@
-import { Box, Button, Stack, Text } from '@chakra-ui/react';
+import { Box, Stack, Text, Image, Center } from '@chakra-ui/react';
+import checkMark from '../images/icon-thank-you.svg';
 
 const ThankYou = () => {
   return (
-    <Box>
-      <Text>Thank you!</Text>
-      <Text>
-        Thanks for confirming your subscription! We hope you have fun using our
-        platfrom. If you ever need support, please feel free to email us at
-        support@loremgaming.com.
-      </Text>
-    </Box>
+    <Center m={'auto'} textAlign={'center'} w={'75%'} h={'100%'}>
+      <Stack>
+        <Image src={checkMark} mx={'auto'} />
+        <Stack mt={6}>
+          <Text fontSize={'2xl'} fontWeight={'bold'}>
+            Thank you!
+          </Text>
+          <Text color={'cool-gray'}>
+            Thanks for confirming your subscription! We hope you have fun using
+            our platfrom. If you ever need support, please feel free to email us
+            at support@loremgaming.com.
+          </Text>
+        </Stack>
+      </Stack>
+    </Center>
   );
 };
 
