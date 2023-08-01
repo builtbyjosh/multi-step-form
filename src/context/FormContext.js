c8cc10685583de756ae8d4de1e4939e9cd85c645
builtbyjosh <joshuacollins912@gmail.com>
2023-08-01 10:09:47 -0500

    fix errors bug

--- src/context/FormContext.js
+++ src/context/FormContext.js
@@ -1,13 +1,17 @@
 import React, { createContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { paymentPlans } from '../data/paymentPlans';
 const FormContext = createContext();
 
 const FormProvider = ({ children }) => {
   const [formStep, setFormStep] = useState(1);
-  const { handleSubmit, register, errors, setValue } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
   const [isYearly, setIsYearly] = useState(false);
-  const [selectedPlan, setSelectedPlan] = useState(paymentPlans.monthly[0]);
+  const [selectedPlan, setSelectedPlan] = useState();
   const [addOnCharges, setAddOnCharges] = useState([]);
 
   return (
