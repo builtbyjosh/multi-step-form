4e952bc8b74008fd3a8acba712db1e4b37f1e724
builtbyjosh <joshuacollins912@gmail.com>
2023-08-02 12:25:50 -0500

    trying to fix vertical alignment

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -3,21 +3,28 @@ import checkMark from '../images/icon-thank-you.svg';
 
 const ThankYou = () => {
   return (
-    <Center m={'auto'} textAlign={'center'} w={'75%'} h={'100%'}>
-      <Stack>
+    <Stack
+      mx={'auto'}
+      mt={{ base: 'unset', md: '100px' }}
+      w={'75%'}
+      h={'100%'}
+      align={'center'}
+      justify={'center'}
+    >
+      <Stack textAlign={'center'}>
         <Image src={checkMark} mx={'auto'} />
-        <Stack mt={6}>
+        <Stack mt={6} textAlign={'center'}>
           <Text fontSize={'2xl'} fontWeight={'bold'}>
             Thank you!
           </Text>
           <Text color={'cool-gray'}>
             Thanks for confirming your subscription! We hope you have fun using
-            our platfrom. If you ever need support, please feel free to email us
+            our platform. If you ever need support, please feel free to email us
             at support@loremgaming.com.
           </Text>
         </Stack>
       </Stack>
-    </Center>
+    </Stack>
   );
 };
 
